417a20107a66b6d502244399b8a0dc0566148a28
Gonzalo Ocampo <gonza.ocampo4@gmail.com>
2023-07-05 10:39:14 -0300

    ejercicio2 terminado

--- Mod3Uni4/ejercicio2.js
+++ Mod3Uni4/ejercicio2.js
@@ -1,9 +1,12 @@
+const textar = document.getElementsByTagName('textarea')[0];
+
 function validarCaract() {
     const largoValido = validarLargo(document.form.texto.value, 5)
 
     if (largoValido == false) {
         alert ('El texto ingresado cumple con los requisitos');
     } else {
+        textar.style.color = 'red';
         alert ('El texto ingresado no cumple con el máximo de caracteres');
     }
     
