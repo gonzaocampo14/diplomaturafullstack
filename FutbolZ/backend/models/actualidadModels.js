e75dbce5141e0c4e80ff91388b13fefbae764416
Gonzalo Ocampo <gonza.ocampo4@gmail.com>
2023-08-25 13:26:31 -0300

    agrego actualidad

---
+++ FutbolZ/backend/models/actualidadModels.js
@@ -0,0 +1,28 @@
+var pool = require('./bd');
+
+async function getNovedades() {
+        var query = 'select * from novedades';
+        var rows = await pool.query(query);
+        return rows;
+}
+
+async function insertNovedad(obj) {
+        try {
+                var query = "insert into novedades set ?";
+                var rows = await pool.query(query, [obj])
+                return rows;
+        } catch (error) {
+                console.log(error);
+                throw error;
+        }
+}
+
+async function deleteNovedad(id) {
+        var query = 'delete from novedades where id ?';
+        var rows = await pool.query(query, [id]);
+        return rows;
+}
+
+module.exports = { getNovedades, insertNovedad }
+
+module.exports = { getNovedades, insertNovedad }
--- FutbolZ/backend/routes/admin/actualidad.js
+++ FutbolZ/backend/routes/admin/actualidad.js
@@ -1,12 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModels = require('../../models/actualidadModels');
 
 
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+  var novedades = await novedadesModels.getNovedades();
   res.render('admin/actualidad', {
     layout: 'admin/layout',
-    logueado: req.session.nombre
+    logueado: req.session.nombre,
+    novedades
   });
 });
+
+router.get('/new', (req, res, next) => {
+  res.render('admin/new',{
+    layout: 'admin/layout'
+  })
+})
+
+router.post('/new', async (req, res, next) => {
+  try {
+    
+    console.log(req.body)
+
+    if (req.body.titulo != "" && req.body.equipo != "" && req.body.cuerpo != "") {
+      await novedadesModels.insertNovedad(req.body);
+      res.redirect('/admin/actualidad')
+    } else {
+      res.render('admin/new', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'Complete todos los campos'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.render('admin/new', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'La novedad no se agrego'
+    })
+  }
+})
 
 module.exports = router;
